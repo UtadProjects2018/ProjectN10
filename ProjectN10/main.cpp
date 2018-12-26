6bdabb00fdbf616fd286f6342aa4c7a0a11e1401
ingridwarrior2901 <ingridguerrero@protonmail.com>
2018-12-26 15:03:55 -0500

    added print result

--- ProjectN10/main.cpp
+++ ProjectN10/main.cpp
@@ -66,6 +66,20 @@ int main(int argc, const char * argv[]) {
     }
     
     intValue.Reset();
+    /* Print Result->
+     First value 10, TList Size 1
+     TList Size 2
+     TList Size 3
+     TList Value 10
+     TList Value 20
+     TList Value 30
+     First value 10, TList Size 1
+     TList Size 2
+     TList Size 3
+     TList Value 10
+     TList Value 20
+     TList Value 30
+     */
     
     return 0;
 }
